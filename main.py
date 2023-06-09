cfc13edd9d2786787f891184f03fd7dbf88f27a9
Neil Schark <gitlab@schark.eu>
2023-06-09 19:04:18 +0200

    receive data

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import socket
+
+HOST = "gpn-tron.duckdns.org"  # The server's hostname or IP address
+PORT = 4000  # The port used by the server
+
+
+def receive_data():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(b"join|tronminator|GktQCxssIwvUBOoU\n")
+        while True:
+            print("Waiting for data")
+            data = s.recv(1024)
+            print(data)
+
 def main():
-    print("Hello, World!")
+    print("Bot started")
+    receive_data()
+
 
 
 if __name__ == "__main__":
